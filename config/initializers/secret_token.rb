07889dfb3b875ba999e75a3d7bb1692c57bac508
Heber Garza <heber.fernando@gmail.com>
2013-09-26 20:00:20 -0300

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hack4goodSantiago::Application.config.secret_key_base = '31d7345b0ce721e6581f260759480dca89547c36b192a0992bea7ed958d62456c55aa0bd89daa6b86a4c1dd2ffe2026c87db98a1c751a053bbd952635ec0af38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hack4goodSantiago::Application.config.session_store :cookie_store, key: '_hack4good_santiago_session'
